1c10728279431a3d95bd0c7aa80b0c9ad3d90fa2
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2022-12-04 14:49:00 +0100

    [added] remove from main unused code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
-#include "classes/Coordinate/Coordinate.h"
 #include "classes/Coordinate/Coordinate.cpp"
-#include <iostream>
+
 int main(){
-    auto *pPoint = new Coordinate(10.0, 10);
-    std::cout<<pPoint->to_string()<<std::endl;
     return 0;
 }
