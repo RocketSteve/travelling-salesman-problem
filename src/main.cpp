6e824581803974ec7f5b8de3307a7180d0bd94a3
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2022-12-19 16:06:07 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,6 @@ using namespace std;
 
 
 int main() {
-    Test
-    test = new Test()
-    asfsd;
-    int i = 0;
-    int i = 0;
-    int i = 0;
-    int r = 0;
-    int d = 0;
+    Test test = new Test()    asfsd;
+    int i = 0; int i = 0; int i = 0; int     r = 0; int d = 0;
 }
